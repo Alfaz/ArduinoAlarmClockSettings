aff08a97a845a167fb5c671acee3beb14a2dcef4
Alfaz <alfazahmed@gmail.com>
2015-12-15 10:52:31 +0530

    Create lcd_functions.h

---
+++ lcd_functions.h
@@ -0,0 +1,12 @@
+#define txPin 2
+
+void runningState();
+void setTime();
+void setAlarm();
+void newLine();
+void clearLCD();
+void settingsLoop(int item);
+void initLCD();
+void chillState();
+void backLightOff();
+void backLightOn();
